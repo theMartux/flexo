7515d634886359ec03e65bd01410e28145f10d2b
theMartux <work@davidemartucci.it>
2019-04-29 19:31:30 +0200

    add test folder

---
+++ test/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+	<meta charset="UTF-8">
+	<title>FLEXO | Test</title>
+	<meta name="viewport"  content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" href="../dist/css/flexo.min.css" />
+	<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	
+</head>
+<body id="wireframe-fill" class="">
+	<main>
+		<div class="container fluid">
+			<div class="row wrap">
+				<div class="fl-box fl-sm-4">
+					<div class="fl-box-in">1</div>
+				</div>
+				<div class="fl-box fl-sm-5">
+					<div class="fl-box-in">2</div>
+				</div>
+				<div class="fl-box fl-sm-4">
+					<div class="fl-box-in">3</div>
+				</div>
+			</div>
+			<br />
+			<div class="row row-reverse wrap">
+				<div class="fl-box fl-sm-4">
+					<div class="fl-box-in">1</div>
+				</div>
+				<div class="fl-box fl-sm-5">
+					<div class="fl-box-in">2</div>
+				</div>
+				<div class="fl-box fl-sm-4">
+					<div class="fl-box-in">3</div>
+				</div>
+			</div>
+			<br />
+			<div class="row wrap-reverse wrap">
+				<div class="fl-box fl-sm-4">
+					<div class="fl-box-in">1</div>
+				</div>
+				<div class="fl-box fl-sm-5">
+					<div class="fl-box-in">2</div>
+				</div>
+				<div class="fl-box fl-sm-4">
+					<div class="fl-box-in">3</div>
+				</div>
+			</div>
+			<div class="row wrap-reverse row-reverse wrap">
+				<div class="fl-box fl-sm-4">
+					<div class="fl-box-in">1</div>
+				</div>
+				<div class="fl-box fl-sm-5">
+					<div class="fl-box-in">2</div>
+				</div>
+				<div class="fl-box fl-sm-4">
+					<div class="fl-box-in">3</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
