70b1247766c594649fae3c68f98d236b6b736415
theMartux <work@davidemartucci.it>
2019-04-29 19:32:10 +0200

    add tutorial folder

---
+++ tutorial/index.php
@@ -0,0 +1,67 @@
+<?php require_once('../PHP/config.php') ?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+	<meta charset="UTF-8">
+	<title>FLEXO | Tutorial</title>
+	<meta name="viewport"  content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" href="../dist/css/flexo.min.css" />
+	<link rel="stylesheet" href="../dist/css/main.css" />
+	<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	
+</head>
+<body id="" class="">
+	<?php require_once('../PHP/partial/header.php') ?>
+	<main>
+		<div class="container fluid">
+			<div class="row wrap block">
+				<div class="fl-box fl-sm-12 fl-md-10 fl-mg-8 fl-md-offset-1 fl-mg-offset-2">
+					<div class="fl-box-in no-padding">
+						<div class="row wrap" id="int">
+							<div class="fl-box fl-sm-12">
+								<div class="fl-box-in">
+									<h1>
+										Tutorial.
+									</h1>
+									<h2>
+										Watch the tutorials and learn Flexo.
+									</h2>
+								</div>
+							</div>
+							<div class="fl-box fl-sm-10 fl-md-8">
+								<div class="fl-box-in">
+									<p>
+										With flexo you can use the wireframe and check the boxes you use during the changing of the device lengths.
+										You should add the wireframe id in the body tag.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="fl-box fl-sm-12">
+					<div class="fl-box-in">
+						<div class="fl-box fl-sm-12">
+							<div class="fl-box-in">
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+	<script src="../dist/js/wireframe.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
