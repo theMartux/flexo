a4dbce9ebd2f5e066e06e1d3196b1f7b26159d2e
theMartux <work@davidemartucci.it>
2019-05-13 12:57:38 +0200

    update customize

--- customize/index.php
+++ customize/index.php
@@ -1,4 +1,12 @@
-<?php require_once('../PHP/config.php') ?>
+<?php 
+session_start();
+require_once('../PHP/config.php');
+require_once('../PHP/functions.php');
+if(isset($_SESSION['name_file']))
+{
+	header("Refresh:0 {$base_url}/PHP/download-customize.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -37,10 +45,18 @@
 									</p>
 								</div>
 							</div>
+							<?php if(isset($_SESSION['error_input']) && $_SESSION['error_input'] == 1): ?>
 							<div class="fl-box fl-sm-10 fl-md-8">
 								<div class="fl-box-in">
-									<a id="customize-min" class="link-int" href="#" title="Use in distribution">No Wireframe</a>
-									<a id="customize-dev" class="link-int" href="#" title="Use in development">Wireframe</a>
+									<p id="error-customize">Not valid inserted values!</p>
+								</div>
+							</div>
+							<?php endif; ?>
+							<div class="fl-box fl-sm-10 fl-md-8">
+								<div class="fl-box-in">
+									<a id="customize-min" class="link-int <?php echo ($_SESSION['form_customize']['dev_min'] == 'min') ? 'checked' : '' ?>" 
+									   href="#" title="Use in distribution">No Wireframe *</a>
+									<a id="customize-dev" class="link-int <?php echo ($_SESSION['form_customize']['dev_min'] == 'dev') ? 'checked' : '' ?>" href="#" title="Use in development">Wireframe *</a>
 								</div>
 							</div>
 							<div class="fl-box  fl-sm-12" id="customize">
@@ -49,12 +65,12 @@
 										Compile the form and download
 									</h3>
 									<form method="post" action="../PHP/compiler-scss.php" id="form-customize">
-										<input type="hidden" name="dev_min" id="dev-min" value="" />
+										<input type="hidden" name="dev_min" id="dev-min" <?php csz_sess('dev_min'); ?> />
 										<div class="row wrap fluid row-input">
 											<div class="fl-box fl-sm-6 fl-lg-2">
 												<div class="fl-box-in no-padding">
-													<label>Box number</label>
-													<input class="input" type="text" name="box" placeholder="Default is 12"/>
+													<label>Box number *</label>
+													<input class="input" type="text" name="box" placeholder="Default is 12" <?php csz_sess('box'); ?>/>
 												</div>
 											</div>
 											<div class="fl-box fl-sm-12">
@@ -64,32 +80,32 @@
 										<div class="row wrap fluid row-input">
 											<div class="fl-box fl-sm-6 fl-lg-3">
 												<div class="fl-box-in no-padding">
-													<label>Media query MD</label>
-													<input class="input" type="text" name="media-query-md" placeholder="Default is 780px" />
+													<label>Media query MD*</label>
+													<input class="input" type="text" name="media-query-md" placeholder="Default is 780px" <?php csz_sess('media-query-md'); ?>/>
 												</div>
 											</div>
 											<div class="fl-box fl-sm-6 fl-lg-3">
 												<div class="fl-box-in no-padding">
-													<label>Media query LG</label>
-													<input class="input" type="text" name="media-query-lg" placeholder="Default is 1240px" />
+													<label>Media query LG *</label>
+													<input class="input" type="text" name="media-query-lg" placeholder="Default is 1240px" <?php csz_sess('media-query-lg'); ?> />
 												</div>
 											</div>
 											<div class="fl-box fl-sm-6 fl-lg-3">
 												<div class="fl-box-in no-padding">
-													<label>Media query SP</label>
-													<input class="input" type="text" name="media-query-sp" placeholder="Default is 1460px" />
+													<label>Media query SP *</label>
+													<input class="input" type="text" name="media-query-sp" placeholder="Default is 1460px" <?php csz_sess('media-query-sp'); ?> />
 												</div>
 											</div>
 											<div class="fl-box fl-sm-6 fl-lg-3">
 												<div class="fl-box-in no-padding">
-													<label>Media query MG</label>
-													<input class="input" type="text" name="media-query-mg" placeholder="Default is 1680px" />
+													<label>Media query MG *</label>
+													<input class="input" type="text" name="media-query-mg" placeholder="Default is 1680px" <?php csz_sess('media-query-mg'); ?> />
 												</div>
 											</div>
 											<div class="fl-box fl-sm-6 fl-lg-3">
 												<div class="fl-box-in no-padding">
-													<label>Media query UT</label>
-													<input class="input" type="text" name="media-query-ut" placeholder="Default is 1920px" />
+													<label>Media query UT *</label>
+													<input class="input" type="text" name="media-query-ut" placeholder="Default is 1920px" <?php csz_sess('media-query-ut'); ?> />
 												</div>
 											</div>
 											<div class="fl-box fl-sm-12">
@@ -99,14 +115,14 @@
 										<div class="row wrap fluid row-input">
 											<div class="fl-box fl-sm-6 fl-lg-4">
 												<div class="fl-box-in no-padding">
-													<label>Padding box</label>
-													<input class="input" type="text" name="padding-box" placeholder="Default 3px 3px" />
+													<label>Padding box *</label>
+													<input class="input" type="text" name="padding-box" placeholder="Default 3px 3px" <?php csz_sess('padding-box'); ?> />
 												</div>
 											</div>
 											<div class="fl-box fl-sm-6 fl-lg-4">
 												<div class="fl-box-in no-padding">
-													<label>Padding box in</label>
-													<input class="input" type="text" name="padding-box-in" placeholder="Default is 10px 10px" />
+													<label>Padding box in *</label>
+													<input class="input" type="text" name="padding-box-in" placeholder="Default is 10px 10px" <?php csz_sess('padding-box-in'); ?> />
 												</div>
 											</div>
 											<div class="fl-box fl-sm-12">
@@ -116,37 +132,46 @@
 										<div class="row wrap fluid row-input">
 											<div class="fl-box fl-sm-4 fl-lg-3 fl-mg-2">
 												<div class="fl-box-in no-padding">
-													<label>Transition</label>
-													<input class="input" type="text" name="transition" placeholder="Default 1" />
+													<label>Transition *</label>
+													<input class="input" type="text" name="transition" placeholder="Default 1" <?php csz_sess('transition'); ?> />
 												</div>
 											</div>
 											<div class="fl-box fl-sm-4 fl-lg-3">
 												<div class="fl-box-in no-padding">
-													<label>Transition time</label>
-													<input class="input" type="text" name="transition-time" placeholder="Default 300ms" />
+													<label>Transition time *</label>
+													<input class="input" type="text" name="transition-time" placeholder="Default 300ms" <?php csz_sess('transition-time'); ?> />
 												</div>
 											</div>
 											<div class="fl-box fl-sm-4 fl-lg-3">
 												<div class="fl-box-in no-padding">
-													<label>Transition easing</label>
-													<input class="input" type="text" name="transition-easing" placeholder="Default ease-in-out" />
+													<label>Transition easing *</label>
+													<input class="input" type="text" name="transition-easing" placeholder="Default ease-in-out" <?php csz_sess('transition-easing'); ?> />
 												</div>
+											</div>
+											<div class="fl-box fl-sm-12">
+												<p>Set the transition. | <span>Default: transition(1), duration(300ms), easing(ease-in-out)</span></p>
 											</div>
 										</div>
 										<div class="row wrap fluid row-input">
 											<div class="fl-box fl-sm-4 fl-lg-3 fl-mg-2">
 												<div class="fl-box-in no-padding">
-													<label>Overflow X hidden</label>
-													<input class="input" type="text" name="overflow-x" placeholder="Default 1" />
+													<label>Overflow X hidden *</label>
+													<input class="input" type="text" name="overflow-x" placeholder="Default 1" <?php csz_sess('overflow-x'); ?> />
 												</div>
 											</div>
+											<div class="fl-box fl-sm-12">
+												<p>Set the Overflow X for the body/html tag. | <span>Default: 1</span></p>
+											</div>
 										</div>
-										<div class="row wrap fluid row-input">
+										<div class="row wrap fluid row-input no-border">
 											<div class="fl-box fl-sm-6 fl-lg-4 fl-sp-3 fl-mg-2">
 												<div class="fl-box-in no-padding">
-													<label>Levels z-index</label>
-													<input class="input" type="text" name="level-z-index" placeholder="Default 12" />
+													<label>Levels z-index *</label>
+													<input class="input" type="text" name="level-z-index" placeholder="Default 12" <?php csz_sess('level-z-index'); ?> />
 												</div>
+											</div>
+											<div class="fl-box fl-sm-12">
+												<p>Set the number of z-index level. | <span>Default: 12</span></p>
 											</div>
 										</div>
 										<div class="row wrap fluid">
@@ -169,7 +194,9 @@
 	<script src="../dist/js/customize.js"></script>
 </body>
 </html>
+<?php 
 
+?>
 
 
 
